b8a7d64d937543be310dbeff7bd42401bef99d75
hoon8141 <younghoon8141@daum.net>
2021-02-03 17:40:27 +0900

    scroll to section

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 'use strict';
 
+//MAKE NAVBAR
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 addEventListener('scroll', () => {
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
@@ -11,3 +11,18 @@ addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// Handle navbar_menu
+const navbarMenu = navbar.querySelector('.navbar__menu');
+addEventListener('click', (event) =>{
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    else{
+        const scrollToId = document.querySelector(link);
+        scrollToId.scrollIntoView({behavior: "smooth"});
+        
+    }
+})
