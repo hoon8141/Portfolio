d61eb5791c077b829cf17eceb7522015673529d7
hoon8141 <younghoon8141@daum.net>
2021-02-10 15:58:13 +0900

    final fix

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 //MAKE NAVBAR
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -11,7 +10,6 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
-
 // Handle navbar_menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) =>{
@@ -20,28 +18,22 @@ navbarMenu.addEventListener('click', (event) =>{
     if(link == null){
         return;
     }
-    navbarMenu.classList.remove('open');
-    scrollIntoView(link);
-    selectNavItem(target);
-
+    else{
+        navbarMenu.classList.remove('open')
+        scrollIntoView(link);
+        
+    }
 });
-
 // toggle button
 const toggle = document.querySelector('.toggle');
 toggle.addEventListener('click', ()=>{
     navbarMenu.classList.toggle('open');
 });
-
 //Handle home button
 const homebutton = document.querySelector('.home__button');
 homebutton.addEventListener('click', (event) =>{
     scrollIntoView('#contact');
 });
-
-function scrollIntoView(selector){
-    const scrollToId = document.querySelector(selector);
-    scrollToId.scrollIntoView({behavior: "smooth"});
-}
 
 //Transparent Home
 const homeContainer = document.querySelector('.home__container');
@@ -49,7 +41,6 @@ const homeHeight = homeContainer.getBoundingClientRect().height;
 document.addEventListener('scroll', () =>{
     homeContainer.style.opacity = 1 - window.scrollY / homeHeight ;
 });
-
 //Arrow up Button
 const arrowbtn = document.querySelector('.arrow-up');
 document.addEventListener('scroll', ()=>{
@@ -60,12 +51,10 @@ document.addEventListener('scroll', ()=>{
         arrowbtn.classList.remove('visible');
     }
 });
-
 // Aroow up Button Click
 arrowbtn.addEventListener('click', ()=>{
     scrollIntoView('#home');
 });
-
 // projcet fillter
 const workBtnContainer = document.querySelector('.work__categories');
 const projcetContainer = document.querySelector('.work__projects');
@@ -75,13 +64,10 @@ workBtnContainer.addEventListener('click', (e)=>{
     if(filter === null){
         return ;
     }
-
     const select = document.querySelector('.categories__btn.selected');
     select.classList.remove('selected');
     const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
     target.classList.add('selected');
-
-
     projcetContainer.classList.add('anim-out');
     setTimeout(() => {
         projects.forEach(project => {   
@@ -91,11 +77,9 @@ workBtnContainer.addEventListener('click', (e)=>{
                 project.classList.add('invisible');
             }
         });
-
         projcetContainer.classList.remove('anim-out');
     }, 300);
 });
-
 const sectionIds = [
     '#home',
     '#about',
@@ -108,44 +92,49 @@ const sections = sectionIds.map(id => document.querySelector(id));
 const navItems = sectionIds.map(id => 
     document.querySelector(`[data-link="${id}"]`)
 );
-
-let selectedNavIndex = 0;
 let selectedNavItem = navItems[0];
-function selectNavItem(selected) {
-  selectedNavItem.classList.remove('active');
-  selectedNavItem = selected;
-  selectedNavItem.classList.add('active');
+let selectedNavIndex = 0;
+function selectNavItem(selected){
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
+function scrollIntoView(selector){
+    const scrollToId = document.querySelector(selector);
+    scrollToId.scrollIntoView({behavior: "smooth"});
+    selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
 
 const observerOptions = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.3,
+    root : null,
+    rootMargin : '0px',
+    threshold : 0.3,
 };
-
 const observerCallback = (entries, observer) => {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
-      const index = sectionIds.indexOf(`#${entry.target.id}`);
-      // 스크롤링이 아래로 되어서 페이지가 올라옴
-      if (entry.boundingClientRect.y < 0) {
-        selectedNavIndex = index + 1;
-      } else {
-        selectedNavIndex = index - 1;
-      }
-    }
-  });
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+            
+            if (entry.boundingClientRect.y < 0 ) {
+                selectedNavIndex = index + 1;
+            }else {
+                selectedNavIndex = index - 1;
+            }
+        }
+    });
 };
-
 
 const observer = new IntersectionObserver(observerCallback, observerOptions);
 sections.forEach(section => observer.observe(section));
 
-window.addEventListener('wheel', () => {
+window.addEventListener('wheel', ()=> {
     if(window.scrollY === 0){
         selectedNavIndex = 0;
-    }else if(Math.round(window.scrollY + window.innerHeight) === document.body.clientHeight){
-        selectedNavIndex = 5;
+    }else if(Math.round(window.scrollY + innerHeight) === document.body.clientHeight){
+        selectedNavIndex = navItems.length - 1;
     }
     selectNavItem(navItems[selectedNavIndex]);
+
+
 });
